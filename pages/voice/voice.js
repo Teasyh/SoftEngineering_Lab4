458cba3e1546bf5e4d50496ea5677d5593a3ba2b
Teasyh <525116790@qq.com>
2021-10-08 19:10:07 +0800

    添加背景音乐

--- pages/voice/voice.js
+++ pages/voice/voice.js
@@ -3,13 +3,13 @@ backgroundAudioManager.title = '背景音乐'
 backgroundAudioManager.singer = '中秋博饼'
 backgroundAudioManager.coverImgUrl = '' //封面图 URL
 // 设置了 src 之后会自动播放（src为云开发中云存储空间文件的链接）
-backgroundAudioManager.src = 'http://music.163.com/song/media/outer/url?id=1383923128.mp3'
+backgroundAudioManager.src = 'http://music.163.com/song/media/outer/url?id=1310733611.mp3'
 var that = backgroundAudioManager
 // 背景音乐循环的方法
 // 1、onEnded监听播放自然结束
 backgroundAudioManager.onEnded(function() {
   // 2、必须重新设置src才能循环之后会自动播放
-  backgroundAudioManager.src = 'http://music.163.com/song/media/outer/url?id=1383923128.mp3'
+  backgroundAudioManager.src = 'http://music.163.com/song/media/outer/url?id=1310733611.mp3'
 })
 Page({
   data: { switchStatus: true },
